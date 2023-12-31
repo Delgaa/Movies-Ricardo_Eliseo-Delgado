49957ae32e9e80d0a68826383e70cee7e9ba4a30
Delgaa <112789365+Delgaa@users.noreply.github.com>
2023-12-31 18:55:33 -0300

    Sprint 1 JS

---
+++ app.js
@@ -0,0 +1,20 @@
+const $movies = document.getElementById("movies")
+console.log($movies)
+
+let createArticle = function (img_p, title_p, tagline_p, descrption_p) {
+    return `<article class = "bg-slate-300 w-[300px] border rounded-2xl h-[600px] flex flex-col items-center gap-4 pt-4">
+    <img class = " w-[250px]" src= "${img_p}">
+    <h2 class = "text-start w-[95%] text-xl font-bold">${title_p}</h2>
+    <h3 class = "text-start w-[95%] font-medium" >${tagline_p}</h3>
+    <p class = "text-start w-[95%] leading-snug">${descrption_p}</p>
+    </article>`
+}
+let passSection = (arrMovies) => {
+    let element = "" 
+    for (const arrMovie of arrMovies) {
+        element += createArticle(arrMovie.image, arrMovie.title, arrMovie.tagline, arrMovie.overview)
+    }
+    return element
+}
+
+$movies.innerHTML = passSection(movies)
