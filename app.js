267060c4d47eee1ff1e444b5aa5078c14ee41ece
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-01-03 11:24:29 -0300

    Sprint 1 JS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const $movies = document.getElementById("movies")
 
 let createArticle = function (img_p, title_p, tagline_p, descrption_p) {
     return `<article class = "bg-slate-300 w-[300px] border rounded-2xl h-[600px] flex flex-col items-center gap-4 pt-4">
-    <img class = " w-[250px]" src= "${img_p}">
+    <img class = " w-[250px]" src= "${img_p}" alt="image ${title_p}">
     <h2 class = "text-start w-[95%] text-xl font-bold">${title_p}</h2>
     <h3 class = "text-start w-[95%] font-medium" >${tagline_p}</h3>
     <p class = "text-start w-[95%] leading-snug">${descrption_p}</p>
