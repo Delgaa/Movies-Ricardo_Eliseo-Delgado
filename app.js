962a1d78e68dd90894942baad5e911090c5f31fc
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-01-03 16:58:40 -0300

    Sprint 1 JS

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const $movies = document.getElementById("movies")
 
 let createArticle = function (img_p, title_p, tagline_p, descrption_p) {
-    return `<article class = "bg-slate-300 w-[300px] border rounded-2xl h-[600px] flex flex-col items-center gap-4 pt-4">
+    return `<article class = "bg-slate-300 w-[250px] border rounded-2xl h-[400px] flex flex-col items-center gap-4 pt-4 lg:w-[300px]">
     <img class = " w-[250px]" src= "${img_p}" alt="image ${title_p}">
     <h2 class = "text-start w-[95%] text-xl font-bold">${title_p}</h2>
     <h3 class = "text-start w-[95%] font-medium" >${tagline_p}</h3>
-    <p class = "text-start w-[95%] leading-snug">${descrption_p}</p>
+    <p class = "text-start w-[95%] line-clamp-4">${descrption_p}</p>
     </article>`
 }
 let passSection = (arrMovies) => {
