6ea90dd4b81b015590c87d042b0a290e39ab5950
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-01-09 20:18:05 -0300

    Sprint 2 Finalizado

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,20 +1,30 @@
+import {
+    passSection, 
+    filterGenres, 
+    filterName, 
+    repeatsGenre, 
+    optionSelec
+} from '../module/functions.js'
+
 const $movies = document.getElementById("movies")
 
-let createArticle = function (arrMovie_p) {
-    return `<article class = "bg-slate-300 w-[250px] border rounded-2xl h-[400px] flex flex-col items-center gap-4 pt-4 lg:w-[300px]">
-    <img class = " w-[250px]" src= "${arrMovie_p.image}" alt="image of ${arrMovie_p.title}">
-    <h2 class = "text-start w-[95%] text-xl font-bold">${arrMovie_p.title}</h2>
-    <h3 class = "text-start w-[95%] font-medium" >${arrMovie_p.tagline}</h3>
-    <p class = "text-start w-[95%] line-clamp-4">${arrMovie_p.overview}</p>
-    <a class = "self-end mb-1 bg-blue-600 p-1 px-2 text-white rounded-xl" href="../pages/details.html?id=${arrMovie_p.id}">Show more</a>
-    </article>`
-}
-let passSection = (arrMovies) => {
-    let element = "" 
-    for (const arrMovie of arrMovies) {
-        element += createArticle(arrMovie)
-    }
-    return element
-}
+passSection(movies, $movies)
 
-$movies.innerHTML += passSection(movies)
+const $seacher = document.getElementById("searchName")
+const $genreSelec = document.getElementById("selecGener")
+
+$seacher.addEventListener("input", () => {
+    const tiping = $seacher.value
+    const listMoviesGenres = filterGenres(movies, $genreSelec.value)
+    const nameMovies = filterName(listMoviesGenres, tiping)
+    passSection(nameMovies, $movies)
+})
+
+const genresLists = repeatsGenre(movies)
+$genreSelec.innerHTML += genresLists.reduce((acumul, genresList) => acumul += optionSelec(genresList))
+
+$genreSelec.addEventListener("change", () => {
+    const mosviesGenre = $genreSelec.value
+    const listMoviesGenres = filterGenres(movies, mosviesGenre)
+    passSection(listMoviesGenres, $movies)
+})
--- assets/script/details.js
+++ assets/script/details.js
@@ -1,46 +1,19 @@
+import {
+    imgDetails, 
+    infoTable1, 
+    infoTable2
+} from '../module/functions.js'
 
 const urlParams = new URLSearchParams(window.location.search)
-const peliculaId = urlParams.get("id")
+const movieId = urlParams.get("id")
 
-const pelicula = movies.find(movie => movie.id == peliculaId)
-console.log(pelicula);
-
-const mostrarImgDetalles = function () {
-    return `
-    <img src= "${pelicula.image}">
-        <div>
-            <h2 class ="text-white">${pelicula.title.toUpperCase()}</h2>
-            <h3 class ="text-white">${pelicula.tagline}</h3>
-            <h3 class ="text-white">${pelicula.genres}</h3>
-            <p class ="text-white">${pelicula.overview}</p>
-        </div>
-    `
-}
+const movieDetails = movies.find(movie => movie.id == movieId)
 
 const $divDetails = document.getElementById("detailsMovies")
-$divDetails.innerHTML = mostrarImgDetalles()
+$divDetails.innerHTML = imgDetails(movieDetails)
 
-const mostrarDatoTable1 = function () {
-    return`
-    <tr class = "text-white">
-        <td class="border border-solid border-white p-2">${pelicula.original_language}</td>
-        <td class="border border-solid border-white p-2">${pelicula.release_date}</td>
-        <td class="border border-solid border-white p-2">${pelicula.runtime} mins</td>
-        <td class="border border-solid border-white p-2">${pelicula.status}</td>
-    </tr>
-    `
-}
 const $dateTable1 = document.getElementById("dataTable1")
-$dateTable1.innerHTML = mostrarDatoTable1()
+$dateTable1.innerHTML = infoTable1(movieDetails)
 
-const mostrarDatoTable2 = function () {
-    return`
-    <tr class = "text-white">
-        <td class="border border-solid border-white p-2">${pelicula.vote_average.toFixed(2)*10} %</td>
-        <td class="border border-solid border-white p-2">${pelicula.budget}</td>
-        <td class="border border-solid border-white p-2">${pelicula.revenue}</td>
-    </tr>
-    `
-}
 const $dateTable2 = document.getElementById("dataTable2")
-$dateTable2.innerHTML = mostrarDatoTable2()
+$dateTable2.innerHTML = infoTable2(movieDetails)
